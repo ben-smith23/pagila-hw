670b835f52a0d5fbe3c8c53b336dc289105c1963
Ben Smith <bsmith23@cmc.edu>
2024-02-13 13:12:36 -0800

    test

--- sql/10.sql
+++ sql/10.sql
@@ -2,3 +2,9 @@
  * Use a JOIN to list each film and the number of actors who are listed for that film.
  * Use tables film and film_actor.
  */
+
+SELECT film.film_id, film.title, count(*) film_actor.actor_id
+FROM film
+JOIN film_actor ON film.film_id = film_actor.film_id
+GROUP BY film_id;
+
