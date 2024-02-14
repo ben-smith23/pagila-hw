bbf9f791368d10435d7ed15ebf913b14ae368829
Ben Smith <bsmith23@cmc.edu>
2024-02-13 22:11:51 -0800

    hmm

--- sql/14_function.sql
+++ sql/14_function.sql
@@ -9,7 +9,8 @@ SELECT film.title
 FROM film
 JOIN film_category ON film_category.film_id = film.film_id
 JOIN category ON film_category.category_id = category.category_id
-WHERE category.name = cat_name;
+WHERE category.name = cat_name
+ORDER BY film.title ASC;
 $$
 LANGUAGE SQL
 IMMUTABLE
