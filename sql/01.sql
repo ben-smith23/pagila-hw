3dfe960b8f6c40eae9a0ab694aac4e3eda65bc19
Ben Smith <bsmith23@cmc.edu>
2024-02-15 10:49:54 -0800

    finished

--- sql/01.sql
+++ sql/01.sql
@@ -3,6 +3,6 @@
  * Name the column Actor Name, and sort the results alphabetically.
  */
 
-SELECT UPPER(CONCAT(first_name, ' ', last_name)) AS "Actor Name"
+SELECT first_name || ' ' || last_name AS "Actor Name"
 FROM actor
-ORDER BY "Actor Name" ASC;
+ORDER BY "Actor Name";
