b9a5f49b56cb6dab67dcf84c6b1262d428038f7a
Ben Smith <bsmith23@cmc.edu>
2024-02-16 10:16:04 -0800

    redo functions

--- sql/02_function.sql
+++ sql/02_function.sql
@@ -2,11 +2,11 @@
  * Find the actor_id of every actor whose first name starts with the indicated string.
  * Order the results from low to hi.
  */
-CREATE OR REPLACE FUNCTION get_actor_ids(prefix text) RETURNS TABLE(actor_id INTEGER) AS
+CREATE OR REPLACE FUNCTION get_actor_ids(text) RETURNS TABLE(actor_id INTEGER) AS
 $$
 SELECT actor_id
 FROM actor
-WHERE first_name ILIKE prefix || '%'
+WHERE first_name ILIKE $1 || '%'
 ORDER BY actor_id ASC;
 $$
 LANGUAGE SQL
